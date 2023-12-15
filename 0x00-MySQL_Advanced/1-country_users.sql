0bd2d9daa297fc9ff9e60a806a2fa82440712921
phurhard <phurhardeen@gmail.com>
2023-12-15 21:42:01 +0100

    corrected coun3

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,10 @@
 -- Creates a table named users with attr of name, email and country
-DROP TABLE IF EXISTS 'users';
+-- Default country will be US
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VAR(255) NOT NULL UNIQUE,
-    name VAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
