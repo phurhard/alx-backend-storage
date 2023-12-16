0c3469eb900a66e8538bc7e7d9c52cd65108c16d
phurhard <phurhardeen@gmail.com>
2023-12-16 10:06:20 +0100

    i don tire for sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,6 @@
 -- This script lists all bands with glam rock as theri main style
 -- ranked by their longevity
 
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
+FROM metal_bands GROUP BY style='Glam rock' ORDER BY lifespan DESC;
+-- WHERE style=Glam rock;
